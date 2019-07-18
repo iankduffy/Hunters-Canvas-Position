7eb4a22464195b5cbd7fd541369ce95585efb096
Ian Duffy <ian.duffy@shiftcommerce.com>
2019-07-18 20:51:17 +0100

    raven invade

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 const canvasDiv = document.getElementById("js-canvas-rugby")
-const download_button = document.getElementById("btn-download");
+const create_button = document.querySelector('[data-create]');
+const download_button = document.querySelector('[data-download]');
 
-download_button.addEventListener('click', function(e){
+
+create_button.addEventListener('click', (e) => {
   html2canvas(canvasDiv).then(canvas => {
     let dataURL = canvas.toDataURL('image/png');
     download_button.href = dataURL; 
   }) 
 })
 
-let homeTeam = document.querySelectorAll('[name="home"]')
+let homeTeam = document.querySelectorAll('[name="home"]')[0]
+let homeLogo = document.querySelector("[data-home-logo]")
+
+homeLogo.style.backgroundImage = "url(assets/hunters-logo.png)"
+
+let awayTeam = document.querySelectorAll('[name="away"]')[0]
+let awayLogo = document.querySelector("[data-away-logo]")
+
+awayLogo.style.backgroundImage = "url(assets/Ravens.png)"
 
 homeTeam.addEventListener("change", (e) => {
-  
 })
